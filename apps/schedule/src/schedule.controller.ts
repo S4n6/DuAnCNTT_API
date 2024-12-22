71775688b24503e252047708237491bba2f170e8
Sang <52100769@student.tdtu.edu.vn>
2024-12-22 22:39:32 +0700

    feat: test ci cd v2.7.6

--- apps/schedule/src/schedule.controller.ts
+++ apps/schedule/src/schedule.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { ScheduleService } from './schedule.service';
+import { IScheduleResponse } from './schedule.response';
 
 @Controller('api/schedule')
 export class ScheduleController {
   constructor(private readonly scheduleService: ScheduleService) {}
 
   @Get('/:eventId')
-  getSchedule(@Param() eventId: string) {
+  async getSchedule(@Param() eventId: string): Promise<IScheduleResponse> {
     return this.scheduleService.getSchedule(eventId);
   }
 }
--- apps/schedule/src/schedule.service.ts
+++ apps/schedule/src/schedule.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Schedule } from './schedule.schema';
+import { IScheduleResponse, ScheduleResponse } from './schedule.response';
 
 @Injectable()
 export class ScheduleService {
@@ -9,11 +10,11 @@ export class ScheduleService {
     @InjectModel('Schedule') private readonly scheduleModel: Model<Schedule>,
   ) {}
 
-  async getSchedule(eventId: string) {
+  async getSchedule(eventId: string): Promise<IScheduleResponse> {
     try{
 
         const schedule = await this.scheduleModel.findOne({ eventId });
-        return schedule;
+        return new ScheduleResponse(true, 'Schedule found', schedule);
 
     }catch(e){
         console.log(e)
