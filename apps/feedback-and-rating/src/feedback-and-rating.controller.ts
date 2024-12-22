5d1888534218bd182540130e0b584883524b7796
Sang <52100769@student.tdtu.edu.vn>
2024-12-22 10:56:10 +0700

    feat: test ci cd v2.6.5

--- apps/feedback-and-rating/src/feedback-and-rating.controller.ts
+++ apps/feedback-and-rating/src/feedback-and-rating.controller.ts
@@ -1,12 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
 import { FeedbackAndRatingService } from './feedback-and-rating.service';
 
-@Controller()
+@Controller('/api/feedbackAndRating/')
 export class FeedbackAndRatingController {
   constructor(private readonly feedbackAndRatingService: FeedbackAndRatingService) {}
 
-  @Get()
-  getHello(): string {
-    return this.feedbackAndRatingService.getHello();
-  }
+  
 }
