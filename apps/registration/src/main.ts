ceff2fb29456fce5e3af5d99e32f7803f16cb036
Sang <52100769@student.tdtu.edu.vn>
2024-12-22 10:28:32 +0700

    feat: test ci cd v2.6.2

--- apps/registration/src/main.ts
+++ apps/registration/src/main.ts
@@ -1,9 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { RegistrationModule } from './registration.module';
 import { REGISTRATION_CONSTANT } from './constant';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { rabbitmqConfig } from './config/rabbitmq.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(RegistrationModule);
   await app.listen(REGISTRATION_CONSTANT.PORT);
+  console.log(`Registration service is running on: ${await app.getUrl()}`);
+
+  const microserviceOptions: MicroserviceOptions = {
+    transport: Transport.RMQ,
+    options: rabbitmqConfig.options,
+  };
+  const microservice = await NestFactory.createMicroservice(
+    RegistrationModule,
+    microserviceOptions,
+  );
+
+  app.connectMicroservice(microserviceOptions);
+  await microservice.listen();
 }
 bootstrap();
