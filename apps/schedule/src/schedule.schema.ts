3125ab36d8164b7a1b35d0d2e61cc8a510c373fd
Sang <52100769@student.tdtu.edu.vn>
2024-12-22 22:10:14 +0700

    feat: test ci cd v2.7.2

---
+++ apps/schedule/src/schedule.schema.ts
@@ -0,0 +1,27 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type ScheduleDocument = Schedule & Document;
+
+@Schema()
+export class Schedule {
+  @Prop({ required: true })
+  eventId: string;
+
+  @Prop({ required: true })
+  title: string;
+
+  @Prop({ required: true })
+  description: string;
+
+  @Prop({ required: true })
+  startTime: Date;
+
+  @Prop({ required: true })
+  endTime: Date;
+
+  @Prop({ required: true })
+  resources: string;
+}
+
+export const ScheduleSchema = SchemaFactory.createForClass(Schedule);
