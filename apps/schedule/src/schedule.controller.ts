3cd080f6d9f0785b70a1ab126edaa544831117ff
Sang <52100769@student.tdtu.edu.vn>
2024-12-22 22:49:25 +0700

    feat: test ci cd v2.7.7

--- apps/schedule/src/schedule.controller.ts
+++ apps/schedule/src/schedule.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { ScheduleService } from './schedule.service';
 import { IScheduleResponse } from './schedule.response';
+import { ScheduleRequest } from './schedule.request';
 
 @Controller('api/schedule')
 export class ScheduleController {
@@ -10,4 +11,25 @@ export class ScheduleController {
   async getSchedule(@Param() eventId: string): Promise<IScheduleResponse> {
     return this.scheduleService.getSchedule(eventId);
   }
+
+  @Post()
+  async createSchedule(
+    @Body() schedule: ScheduleRequest,
+  ): Promise<IScheduleResponse> {
+    return this.scheduleService.createSchedule(schedule);
+  }
+
+  @Put('')
+  async updateSchedule(
+    @Body() schedule: ScheduleRequest,
+  ): Promise<IScheduleResponse> {
+    return this.scheduleService.updateSchedule(schedule);
+  }
+
+  @Delete('/:eventId')
+  async deleteSchedule(@Param('eventId') eventId: string): Promise<void> {
+    return this.scheduleService.deleteSchedule(eventId);
+  }
+
+
 }
--- apps/schedule/src/schedule.service.ts
+++ apps/schedule/src/schedule.service.ts
@@ -3,6 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Schedule } from './schedule.schema';
 import { IScheduleResponse, ScheduleResponse } from './schedule.response';
+import { ScheduleRequest } from './schedule.request';
 
 @Injectable()
 export class ScheduleService {
@@ -16,6 +17,37 @@ export class ScheduleService {
       return new ScheduleResponse(true, 'Schedule found', schedule);
     } catch (e) {
       return new ScheduleResponse(false, e.message, null);
+    }
+  }
+
+  async createSchedule(schedule: ScheduleRequest): Promise<IScheduleResponse> {
+    try {
+      const newSchedule = new this.scheduleModel(schedule);
+      await newSchedule.save();
+      return new ScheduleResponse(true, 'Schedule created', newSchedule);
+    } catch (e) {
+      return new ScheduleResponse(false, e.message, null);
+    }
+  }
+
+  async updateSchedule(schedule: ScheduleRequest): Promise<IScheduleResponse> {
+    try {
+      await this.scheduleModel.updateOne(
+        { eventId: schedule.eventId },
+        schedule,
+      );
+      return new ScheduleResponse(true, 'Schedule updated');
+    } catch (e) {
+      return new ScheduleResponse(false, e.message);
+    }
+  }
+
+  async deleteSchedule(eventId: string): Promise<IScheduleResponse> {
+    try {
+      await this.scheduleModel.deleteOne({ eventId });
+      return new ScheduleResponse(true, 'Schedule deleted');
+    } catch (e) {
+      return new ScheduleResponse(false, e.message);
     }
   }
 }
